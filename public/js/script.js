7af84c92e90931a814c59039e052149ffd3463f8
Anderson Camargo <andergtk@gmail.com>
2016-05-24 00:01:32 -0300

    Atualiza style.css e script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
-  // MODALS
+  /**
+   * Modals
+   */
+
   // Informações do item
   $('.table tbody tr').on('click', function() {
     var itemId = $(this).attr('data-item-id');
@@ -11,28 +14,26 @@ $(document).ready(function() {
   $('.item-edit').on('click', function() {
     var itemId = $(this)
       .closets('tr')
-      .attr('data-item-id');
+        .attr('data-item-id');
     console.log('Item selecionado:', itemId);
   });
 
   // Remover item
   $('.item-delete').on('click', function() {
     var itemId = $(this)
-      .closets('tr')
-      .attr('data-item-id');
+      .closest('tr')
+        .attr('data-item-id');
     var itemDescricao = $(this)
       .closest('tr')
       .find('.item-descricao')
-      .text();
+        .text();
 
-    $('#modal-item-delete').modal();
-    $('#modal-item-delete .item-descricao').text(itemDescricao);
-    $('#modal-item-delete').on('shown.bs.modal', function() {
-      $('#modal-item-delete .delete-link')
-        .attr('href', '/item/delete/' + itemId)
-        .focus();
-      console.log('/item/delete/${itemId}');
-    });
+    $('#modal-item-delete .item-descricao').text('"' + itemDescricao + '"');
+    $('#modal-item-delete').modal()
+      .on('shown.bs.modal', function() {
+        $(this).find('input[name="id"]').attr('value', itemId);
+        $(this).find('.delete-link').focus();
+      });
   });
 
 });
