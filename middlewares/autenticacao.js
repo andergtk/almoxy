454da39ced016f178698292901e037bb471ec759
Anderson Camargo <andergtk@gmail.com>
2016-05-23 23:44:25 -0300

    Adiciona comentários nos middlewares

--- middlewares/autenticacao.js
+++ middlewares/autenticacao.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * Autenticação do usuário
+ */
 module.exports = (req, res, next) => {
   console.log('Autenticando usuário...');
   next();
--- middlewares/validacao.js
+++ middlewares/validacao.js
@@ -1,10 +1,16 @@
 'use strict';
 
+/**
+ * Valida informações do item
+ */
 exports.item = (req, res, next) => {
   console.log('Validando dados do item...');
   next();
 }
 
+/**
+ * Valida informações do usuário
+ */
 exports.usuario = (req, res, next) => {
   console.log('Validando dados do usuario...');
   next();
