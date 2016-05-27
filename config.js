9559f7c0c9c4b7ba79a53cc1805bba2756b587ea
Anderson Camargo <andergtk@gmail.com>
2016-05-26 22:33:54 -0300

    Adiciona variávels do ambiente utilizado

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-  port: process.env.PORT || 3000
+  env: process.env.ENVIRONMENT || 'production'
+, port: process.env.PORT || 3000
 , dbUri: 'mongodb://localhost/almoxy'
 , cookieSecret: '2aEhAr5doPVwiCkdLGx8pcG5jTvmPodY2eM3SmbMc4xgNzgp'
 , sessionSecret: 'oU44aFlsXMtwpgzrKklBdmpfYQOeKBerUONsQXcShQhwMuvg'
