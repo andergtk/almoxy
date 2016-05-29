c68a20ddd49da319f625c22a703eefcbfb76685e
Anderson Camargo <andergtk@gmail.com>
2016-05-28 23:54:51 -0300

    Modal de informações do item funcionando

--- public/js/modals.js
+++ public/js/modals.js
@@ -4,6 +4,30 @@
 $('.table tbody tr').on('click', function() {
   var itemId = $(this).attr('data-item-id');
 
+  $.getJSON('/item/info/' + itemId)
+    .fail(function() {
+      $('#modal-item-info .modal-body')
+        .html('<p>Falha ao buscar informações do item</p>');
+    })
+    .done(function(item) {
+      if (item.erro) {
+        $('#modal-item-info .modal-body')
+          .html('<p>' + item.erro + '</p>');
+        return;
+      }
+
+      if ('almoxarifado' === item.tipo) {
+        $('#modal-item-info .item-tipo').text('Almoxarifado');
+      } else if ('achados-e-perdidos' === item.tipo){
+        $('#modal-item-info .item-tipo').text('Achados e perdidos');
+      }
+
+      $('#modal-item-info .item-data').text(item.data);
+      $('#modal-item-info .item-descricao').text(item.descricao);
+      $('#modal-item-info .item-comentario').text(item.comentario);
+      $('#modal-item-info .item-quantidade').text(item.quantidade);
+    });
+
   $('#modal-item-info').modal()
     .on('shown.bs.modal', function() {
       $(this)
@@ -17,7 +41,7 @@ $('.table tbody tr').on('click', function() {
  */
 $('.item-editar').on('click', function(e) {
   var itemId = $(this)
-    .closest('tr')
+    .closest('[data-item-id]')
       .attr('data-item-id');
 
   e.stopPropagation();
@@ -35,7 +59,7 @@ $('.item-editar').on('click', function(e) {
  */
 $('.item-excluir').on('click', function(e) {
   var itemId = $(this)
-    .closest('tr')
+    .closest('[data-item-id]')
       .attr('data-item-id');
   var itemDescricao = 'Item 007';
 
