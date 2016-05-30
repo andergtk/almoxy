4ec581fbd194445d46c424a95ce4951180463cec
Anderson Camargo <andergtk@gmail.com>
2016-05-29 23:58:26 -0300

    Cria objeto específico para o banco de dados

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
-'use strict';
-
 module.exports = {
-  env: process.env.ENVIRONMENT || 'production'
+  env: process.env.NODE_ENV || 'production'
 , port: process.env.PORT || 3000
-, dbUri: 'mongodb://localhost/almoxy'
-, cookieSecret: '2aEhAr5doPVwiCkdLGx8pcG5jTvmPodY2eM3SmbMc4xgNzgp'
-, sessionSecret: 'oU44aFlsXMtwpgzrKklBdmpfYQOeKBerUONsQXcShQhwMuvg'
+, cookie_secret: '2aEhAr5doPVwiCkdLGx8pcG5jTvmPodY2eM3SmbMc4xgNzgp'
+, session_secret: 'oU44aFlsXMtwpgzrKklBdmpfYQOeKBerUONsQXcShQhwMuvg'
+, db: {
+    uri: 'mongodb://localhost/almoxy'
+  }
 }
