476860d261b7d01f647aebf16977a375f118f487
Anderson Camargo <andergtk@gmail.com>
2016-05-30 21:35:04 -0300

    Cria sub-objeto para as secrets

--- config/index.js
+++ config/index.js
@@ -1,9 +1,13 @@
+'use strict';
+
 module.exports = {
   env: process.env.NODE_ENV || 'production'
 , port: process.env.PORT || 3000
-, cookie_secret: '2aEhAr5doPVwiCkdLGx8pcG5jTvmPodY2eM3SmbMc4xgNzgp'
-, session_secret: 'oU44aFlsXMtwpgzrKklBdmpfYQOeKBerUONsQXcShQhwMuvg'
 , db: {
     uri: 'mongodb://localhost/almoxy'
+  }
+, secret: {
+    cookie: '2aEhAr5doPVwiCkdLGx8pcG5jTvmPodY2eM3SmbMc4xgNzgp'
+  , session: 'oU44aFlsXMtwpgzrKklBdmpfYQOeKBerUONsQXcShQhwMuvg'
   }
 }
