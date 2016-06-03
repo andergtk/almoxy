538cbbdc6b1aca01bf0d0d77cd222cfdf2f6b198
Anderson Camargo <andergtk@gmail.com>
2016-06-03 17:27:18 -0300

    Atualiza arquivos de configurações

--- config/db.js
+++ config/db.js
@@ -2,32 +2,30 @@
 
 const mongoose = require('mongoose');
 
-module.exports = (uri) => {
-  /**
-   * Conexão com o banco de dados
-   */
-  mongoose.connect(uri);
-
-  /**
-   * Eventos
-   */
-  mongoose.connection.on('connected', () => {
-    console.log('[Mongoose] Conectado ao banco de dados.');
-  })
-
-  mongoose.connection.on('disconnected', () => {
-    console.log('[Mongoose] Desconectado do banco de dados.');
-  });
-
-  mongoose.connection.on('error', (err) => {
-    console.log('[Mongoose] Falha na conexão com o banco de dados.');
-    throw err;
-  });
-
-  process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-      console.log('[Mongoose] Conexão encerrada.');
-      process.exit(0);
-    });
-  });
+module.exports = (URL) => {
+  // Conecta ao banco de dados
+  mongoose.connect(URL);
 }
+
+/**
+ * Eventos
+ */
+mongoose.connection.on('connected', () => {
+  console.log('[Mongoose] Conectado ao banco de dados.');
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('[Mongoose] Desconectado do banco de dados.');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('[Mongoose] Falha na conexão com o banco de dados.');
+  throw err;
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('[Mongoose] Conexão encerrada.');
+    process.exit(0);
+  });
+});
--- config/server.js
+++ config/server.js
@@ -1,13 +1,27 @@
 'use strict';
 
-module.exports = {
-  env: process.env.NODE_ENV || 'production'
-, port: process.env.PORT || 3000
-, db: {
-    uri: 'mongodb://localhost/almoxy'
-  }
-, secret: {
-    cookie: '2aEhAr5doPVwiCkdLGx8pcG5jTvmPodY2eM3SmbMc4xgNzgp'
-  , session: 'oU44aFlsXMtwpgzrKklBdmpfYQOeKBerUONsQXcShQhwMuvg'
-  }
+// Opções do banco de dados
+const db = {
+  URL: 'mongodb://localhost/almoxy'
 }
+
+// Secrets
+const secrets = {
+  // Assinatura usada nos cookies
+  cookie: '2aEhAr5doPVwiCkdLGx8pcG5jTvmPodY2eM3SmbMc4xgNzgp'
+
+  // Assinatura dos cookies usados na sessão
+, session: 'oU44aFlsXMtwpgzrKklBdmpfYQOeKBerUONsQXcShQhwMuvg'
+}
+
+// Título base do sistema
+exports.title = 'Almoxy';
+
+// Ambiente
+exports.env = process.env.NODE_ENV || 'production';
+
+// Porta
+exports.port = process.env.PORT || 3000;
+
+exports.db = db;
+exports.secrets = secrets;
