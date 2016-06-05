f610a77295f8fdc3105201360e82056b53e0669a
Anderson Camargo <andergtk@gmail.com>
2016-06-04 23:09:34 -0300

    Adiciona mais variáveis locais de título

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,23 +1,24 @@
 'use strict';
 
 /**
- * 404
+ * Erro 404.
  */
 exports.e404 = (req, res, next) => {
   let err = new Error('O recurso requisitado não foi encontrado.');
 
   err.status = 404;
   err.name = 'Não encontrado';
+
   next(err);
 }
 
 /**
- * Renderiza a página de erros
+ * Renderiza a página de erros.
  */
 exports.errorHandler = (err, req, res, next) => {
   res.status(err.status || 500);
+  res.title(err.name || 'Erro interno do servidor');
   res.render('error', {
-    title: err.name || 'Erro interno do servidor'
-  , message: err.message || 'Erro no servidor ao processar a solicitação.'
+    message: err.message || 'Erro no servidor ao processar a solicitação.'
   });
 }
--- middlewares/title.js
+++ middlewares/title.js
@@ -2,10 +2,12 @@
 
 module.exports = (format) => {
   format = format || '%title% - %base%';
-  res.locals.titleBase = req.app.get('title');
 
   return (req, res, next) => {
     res.title = (title) => {
+      res.locals.titleBase = req.app.get('title');
+      res.locals.titlePage = title || 'Sem título';
+
       title = format.replace(/%title%/ig, title || 'Sem título');
       title = title.replace(/%base%/ig, req.app.get('title'));
       res.locals.title = title;
